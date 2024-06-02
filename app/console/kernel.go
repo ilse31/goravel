652efbac7231008149709b2b5f130c818756d8d3
ilse31 <ilhamprazsetyo33@gmail.com>
2024-06-02 21:03:03 +0700

    setup cron job to update status user

--- app/console/kernel.go
+++ app/console/kernel.go
@@ -1,15 +1,31 @@
 package console
 
 import (
+	"fmt"
+	"goravel/app/models"
+
 	"github.com/goravel/framework/contracts/console"
 	"github.com/goravel/framework/contracts/schedule"
+	"github.com/goravel/framework/facades"
 )
 
 type Kernel struct {
 }
 
 func (kernel *Kernel) Schedule() []schedule.Event {
-	return []schedule.Event{}
+	return []schedule.Event{
+		facades.Schedule().Call(func() {
+			fmt.Println("This is a scheduled task")
+			facades.Log().Info("This is a scheduled task")
+			users := make([]models.User, 0)
+
+			facades.Orm().Query().Model(users).Where("status = ?", "nonactive").Find(&users)
+
+			for _, user := range users {
+				facades.Orm().Query().Model(&user).Update("status", "active")
+			}
+		}).EveryMinute(),
+	}
 }
 
 func (kernel *Kernel) Commands() []console.Command {
--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	// This bootstraps the framework and gets it ready for use.
 	bootstrap.Boot()
 
+	go facades.Schedule().Run()
+
 	//Start http server by facades.Route().
 	go func() {
 		if err := facades.Route().Run(); err != nil {
